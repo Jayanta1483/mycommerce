c282197566977a102f118958862aa74823664f90
Jayanta <nemojoy2001@gmail.com>
2020-12-10 11:01:44 +0530

    config delete for catagories

--- admin/delete.php
+++ admin/delete.php
@@ -1,9 +1,49 @@
 <?php
+session_start();
 require "connection.php";
 
-//#########################################################################################################################################################################################################
+if(isset($_GET["page"]) && $_GET["page"]!== ""){
+    $page = mysqli_real_escape_string($connect, $_GET["page"]);
+    $id = mysqli_real_escape_string($connect, $_GET["id"]);
+   }
 
 //#########################################################################################################################################################################################################
+//                                            CATAGORIES
+//#########################################################################################################################################################################################################
+  if($page === "catagories"){
+    $delete = "DELETE FROM catagories WHERE cat_id = ?";
+    $stmt = mysqli_stmt_init($connect);
+    if(!mysqli_stmt_prepare($stmt, $delete)){
+        echo "<div style='color:red'>SQL Error Occured!!</div>";
+    }else{
+        mysqli_stmt_bind_param($stmt,"i", $id);
+        mysqli_stmt_execute($stmt);
+        header("location:tables.php?page=catagories");
+    }
+  }
+
+  //#########################################################################################################################################################################################################
+  //                                          PRODUCTS
+  //#########################################################################################################################################################################################################
+
+  if($page === "products"){
+    $delete = "DELETE FROM catagories WHERE cat_id = ?";
+    $stmt = mysqli_stmt_init($connect);
+    if(!mysqli_stmt_prepare($stmt, $delete)){
+        echo "<div style='color:red'>SQL Error Occured!!</div>";
+    }else{
+        mysqli_stmt_bind_param($stmt,"i", $id);
+        mysqli_stmt_execute($stmt);
+        header("location:tables.php?page=catagories");
+    }
+  }
+
+
+
+
+
+
+
 
 
 ?>
