ff4fb74cf5592a09f2f3e2cc8a5375e009f3362e
Jayanta <nemojoy2001@gmail.com>
2020-12-10 11:10:39 +0530

    config delete for products and customers

--- admin/delete.php
+++ admin/delete.php
@@ -27,14 +27,46 @@ if(isset($_GET["page"]) && $_GET["page"]!== ""){
   //#########################################################################################################################################################################################################
 
   if($page === "products"){
-    $delete = "DELETE FROM catagories WHERE cat_id = ?";
+    $delete = "DELETE FROM products WHERE product_id = ?";
     $stmt = mysqli_stmt_init($connect);
     if(!mysqli_stmt_prepare($stmt, $delete)){
         echo "<div style='color:red'>SQL Error Occured!!</div>";
     }else{
         mysqli_stmt_bind_param($stmt,"i", $id);
         mysqli_stmt_execute($stmt);
-        header("location:tables.php?page=catagories");
+        header("location:tables.php?page=products");
+    }
+  }
+
+//#########################################################################################################################################################################################################
+//                                          CUSTOMERS
+//#########################################################################################################################################################################################################
+
+  if($page === "customers"){
+    $delete = "DELETE FROM customers WHERE cust_id = ?";
+    $stmt = mysqli_stmt_init($connect);
+    if(!mysqli_stmt_prepare($stmt, $delete)){
+        echo "<div style='color:red'>SQL Error Occured!!</div>";
+    }else{
+        mysqli_stmt_bind_param($stmt,"i", $id);
+        mysqli_stmt_execute($stmt);
+        header("location:tables.php?page=customers");
+    }
+  }
+
+//#########################################################################################################################################################################################################
+  //                                          PRODUCTS
+  //#########################################################################################################################################################################################################
+
+  if($page === "products"){
+    $delete = "DELETE FROM products WHERE product_id = ?";
+    $stmt = mysqli_stmt_init($connect);
+    if(!mysqli_stmt_prepare($stmt, $delete)){
+        echo "<div style='color:red'>SQL Error Occured!!</div>";
+    }else{
+        mysqli_stmt_bind_param($stmt,"i", $id);
+        mysqli_stmt_execute($stmt);
+        header("location:tables.php?page=products");
     }
   }
 
