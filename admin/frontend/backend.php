46eba6c0b33dafffb838d2587ef8dfd411f8b83d
Jayanta <nemojoy2001@gmail.com>
2021-01-01 00:24:33 +0530

    ajax insert

--- admin/frontend/backend.php
+++ admin/frontend/backend.php
@@ -1,17 +1,39 @@
 <?php
 require "connection.php";
 
-if($_POST['op'] == 'insert'){
+if(isset($_POST['op']) && $_POST['op'] == 'insert'){
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $email = $_POST['email'];
     $mobile = $_POST['mobile'];
     $user_id = $_POST['logid'];
-    $pwd = $_POST['pwd'];
+    $pwd = password_hash($_POST['pwd'], PASSWORD_BCRYPT);
     $address = $_POST['adr'];
 
-    
-   
+if(isset($_FILES['file']) && $_FILES['file'] !== ""){
+    $photo = $_FILES['file'];
+    $file_name = $photo['name'];
+}else{
+    $file_name = "customer_avatar.jpg";
+}
+
+  $insert = "INSERT INTO `customers`( `cust_fname`, `cust_lname`, `cust_address`, `cust_email`, `cust_mobile`, `user_id`, `password`, `photo`) VALUES (?,?,?,?,?,?,?,?)";  
+  $stmt = $connect->prepare($insert);
+  $stmt->bind_param("ssssisss",$fname, $lname, $address, $email, $mobile, $user_id, $pwd, $file_name);
+  ($stmt->execute()) ? $execute = true : $execute = false;
+
+  if($execute){
+      $last_id = $connect->insert_id;
+      $select = "select cust_fname from customers where cust_id = ?";
+      $sel_stmt = $connect->prepare($select);
+      $sel_stmt->bind_param("i",$last_id);
+      $sel_stmt->execute();
+      $sel_stmt->bind_result($fn);
+      while($sel_stmt->fetch()){
+          echo json_encode($fn);
+      }
+  }
+
 }else{
     echo "error";
 }
--- admin/frontend/myJquery.js
+++ admin/frontend/myJquery.js
@@ -2,60 +2,63 @@ $(document).ready(function () {
 
     $("#custProfile").click(() => $("#cust-image").trigger("click"))
     $("#cust-image").on("change", (e) => {
-        
+
         let file = e.target.files[0];
         if (file) {
             $("#custProfile").attr("src", window.URL.createObjectURL(file));
-           
-           
-}
-    })
 
 
-$(".p-viewer").click(()=>{
-    if($("#pwd").attr("type")=="password"){
-        $("#pwd").attr("type", "text");
-        $("#eye").css("color", "#33ff33");
-    }else{
-        $("#pwd").attr("type", "password");
-        $("#eye").css("color", "red");
-    }
-})
-
-
-//For Sending Data to Database using Ajax
-console.log($('#cust-image').prop('files')[0])
-
-$("#submit").click(()=>{
-    let imageFile = $('#cust-image').prop('files')[0];
-    
-    let form_data = new FormData();
-    form_data.append('file',imageFile);
-    form_data.append('op','insert')
-    form_data.append('fname',$("#fname").val());
-    form_data.append('lname',$("#lname").val());
-    form_data.append('email',$("#email").val());
-    form_data.append('mobile',$("#mobile").val());
-    form_data.append('logid',$("#logid").val());
-    form_data.append('pwd',$("#pwd").val());
-    form_data.append('adr',$("#adr").val());
-
-    // form_data.forEach(element=>console.log(element))
-    //console.log($('#cust-image').prop('files'))
-    $.ajax({
-        type:"POST",
-        url:"backend.php",
-        contentType:false,
-        processData:false,
-        data:form_data,
-        success:function(response){
-        $("#myForm")[0].reset(); 
-        $("#custProfile").attr("src", "customer_avatar.jpg");
-            alert(response);
         }
-
     })
-})
+
+
+    $(".p-viewer").click(() => {
+        if ($("#pwd").attr("type") == "password") {
+            $("#pwd").attr("type", "text");
+            $("#eye").css("color", "#33ff33");
+        } else {
+            $("#pwd").attr("type", "password");
+            $("#eye").css("color", "red");
+        }
+    })
+
+
+    //For Sending Data to Database using Ajax
+    console.log($('#cust-image').prop('files')[0])
+
+    $("#submit").click(() => {
+        let imageFile = $('#cust-image').prop('files')[0];
+
+        let form_data = new FormData();
+        form_data.append('file', imageFile);
+        form_data.append('op', 'insert')
+        form_data.append('fname', $("#fname").val());
+        form_data.append('lname', $("#lname").val());
+        form_data.append('email', $("#email").val());
+        form_data.append('mobile', $("#mobile").val());
+        form_data.append('logid', $("#logid").val());
+        form_data.append('pwd', $("#pwd").val());
+        form_data.append('adr', $("#adr").val());
+
+        
+        $.ajax({
+            type: "POST",
+            url: "backend.php",
+            contentType: false,
+            processData: false,
+            data: form_data,
+            success: function (response) {
+                if (response !== "error") {
+                    $("#myForm")[0].reset();
+                    $("#custProfile").attr("src", "customer_avatar.jpg");
+                    alert(`Welcome ${JSON.parse(response)} You have Registerd Successfully!!`);
+                } else {
+                    alert("Some Error Occured!!")
+                }
+            }
+
+        })
+    })
 
 
 
